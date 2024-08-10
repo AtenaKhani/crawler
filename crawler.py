1bc3f84f12b928f88d380fedfe03b38b4fe62d97
Atena <atenakhani904@gmail.com>
2024-08-10 15:53:39 +0330

    add async method fetch_pages for requests and extract info from api_pages

--- crawler.py
+++ crawler.py
@@ -11,6 +11,22 @@ class crawler:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
 
+    def fetch_pages(self, session: ClientSession, page: int):
+        async with session.get(f'{self.api_url}?pageIndex={page}', headers=self.headers) as response:
+            try:
+                data = await response.json()
+                if data.get('status') and 'data' in data and 'ads' in data['data']:
+                    return [self.extract_info(ad) for ad in data['data']['ads']]
+                else:
+                    print(f"Data not found on page {page}.")
+            except aiohttp.ContentTypeError:
+                print(f"Error parsing JSON on page {page}. Response content:", await response.text())
+
+            except aiohttp.ClientResponseError as e:
+                print(f"HTTP request error on page {page}: {e}")
+
+    def extract_info(self, ad):
+
 
 
                 
